9e75fe8f42861fbd91f0674ad8609108ca98317c
numso <dosmun@gmail.com>
2013-01-10 09:29:29 -0700

    fixed the calendar

--- public/js/index.js
+++ public/js/index.js
@@ -1,94 +1,142 @@
+var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+var calEvents = [];
+
 $(function () {
+  initializeCalendar();
+  initializeWorkflowy();
+});
 
-  startCalendar();
+function initializeCalendar() {
+  $('#calendar').fullCalendar({
+    header: {
+      left:   'prev,next today',
+      center: 'title',
+      right:  'month,basicWeek,basicDay'
+    }
+  });
+};
 
-  if ($('.wfItems').length) {
-    var groups = [];
-    var candidates = $('.wfItems');
-    for (var i = 0; i < candidates.length; ++i)
-      groups.push($(candidates[i]).data('wfid'));
-    startWorkflowy(groups);
-  }
+function refreshCalendarEvents() {
+  $('#calendar').fullCalendar('removeEvents');
+  $('#calendar').fullCalendar('addEventSource', { events: calEvents });
+  $('#calendar').fullCalendar('rerenderEvents');
+};
+
+function initializeWorkflowy() {
+  if (!$('.wfItems').length)
+    return $('.loading').remove();
+
+  $.get('/getWorkflowy', function (data) {
+    $('.loading').remove();
+    if (!data.success) return alert("Error: invalid workflowy cookie");
+    parseWFEvents(data.workflowy, getGroups());
+    refreshCalendarEvents();
+  });
 
   $('.showHidden').click(function (e) {
     $(this).closest('.list').find('.completed').toggle();
   });
-});
-
-function startCalendar() {
-  $('#calendar').fullCalendar({
-    header: {
-    left:   'prev,next today',
-    center: 'title',
-    right:  'month,basicWeek,basicDay'
-}
-  });
 };
 
-function startWorkflowy(groups) {
-  $.get('/getWorkflowy', function (data) {
-    $('.loading').remove();
-    if (!data.success) return alert("Error: invalid workflowy cookie");
-    populateWorkflowy(data.workflowy, groups);
-  });
-
-  // $('.refresh-wf').click(function () {
-  //   $.get('/getWorkflowy', function (data) {
-  //     if (!data.success) return "Error: tell the user";
-  //     populateWorkflowy(data.workflowy);
-  //   });
-  // });
+function getGroups() {
+  var groups = [];
+  var allLists = $('.wfItems');
+  for (var i = 0; i < allLists.length; ++i)
+    groups.push($(allLists[i]).data('wfid'));
+  return groups;
 };
 
-function populateWorkflowy(wf, groups) {
-  var data = {};
-  for (var i = 0; i < groups.length; ++i)
-    data[groups[i]] = [];
-
-  getTheData(wf, data, groups);
-
-  for (var i = 0; i < groups.length; ++i) {
-    for (var j = 0; j < data[groups[i]].length; ++j) {
-      var item = data[groups[i]][j]
-
-      var dueDate = false;
-      var dashIndex = item.name.indexOf('---');
-      if (dashIndex !== -1) {
-        var dueDate = item.name.substr(dashIndex + 3, item.name.length);
-        item.name = item.name.substr(0, dashIndex);
-      }
-
-      var el = $('<a>')
-        .text(item.name)
-        .attr('href', item.linkName)
-        .addClass(item.className);
-
-      dueDate = dueDate ? '<div>due on ' + dueDate + '</div>' : '';
-
-      el = $('<div>')
-        .append(el)
-        .append(dueDate);
-
-      $('.wfItems[data-wfid="' + groups[i] + '"]').append(el);
-    }
+function parseWFEvents(wf, groups) {
+  for (var i = 0; i < wf.length; ++i) {
+    isTagged(wf[i]);
+    if (wf[i].ch && wf[i].ch.length)
+      parseWFEvents(wf[i].ch, groups);
   }
 
+  function isTagged(item) {
+    for (var i = 0; i < groups.length; ++i)
+      if (item.nm.indexOf(groups[i]) !== -1 && item.nm.indexOf('#no') === -1)
+        addItem(item, groups[i]);
+  };
 };
 
-function getTheData(node, data, groups) {
-  for (var i = 0; i < node.length; ++i) {
+function addItem(item, group) {
+  var sections = item.nm.split('---')
+    , color = grabColor(sections)
+    , start = grabDate(sections)
+    , title = sections.join('---')
+    , completed = !!item.cp;
 
-    for (var j = 0; j < groups.length; ++j) {
-      if (node[i].nm.indexOf(groups[j]) !== -1 && node[i].nm.indexOf('#no') === -1) {
-        data[groups[j]].push({
-          name: node[i].nm,
-          className: !!node[i].cp ? 'completed' : '',
-          linkName: '//www.workflowy.com/#/' + node[i].id
-        });
+  var event = {
+    title: title,
+    allDay: true,
+    start: start,
+    color: color,
+    url: '//www.workflowy.com/#/' + item.id
+  };
+
+  if (event.start)
+    calEvents.push(event);
+
+  $('.wfItems[data-wfid="' + group + '"]').append(renderMyItem(event, completed));
+
+  function grabColor(sections) {
+    for (var i = 1; i < sections.length; ++i) {
+      var section = sections[i];
+      if (section.indexOf('color') !== -1) {
+        section = section.replace('color', '');
+        section = section.trim();
+        sections = sections.splice(i, 1);
+        return section;
       }
     }
+    return undefined;
+  };
 
-    if (node[i].ch && node[i].ch.length)
-      getTheData(node[i].ch, data, groups);
+  function grabDate(sections) {
+    for (var i = 1; i < sections.length; ++i) {
+      var section = sections[i];
+      if (section.indexOf('due') !== -1) {
+        section = section.replace('due', '');
+        section = section.trim();
+        section = dateFromString(section);
+        sections = sections.splice(i, 1);
+        return section;
+      }
+    }
+    return undefined;
+  };
+
+  function renderMyItem(event, completed) {
+    var duedate = !!event.start ? '<div>due on ' + MONTHS[event.start.getMonth()] + " " + event.start.getDate() + '</div>': '';
+    var colorStyle = !!event.color ? ' style="color: ' + event.color + '"' : '';
+    var klass = completed ? ' class="completed"' : '';
+    return '<div' + colorStyle + '><a' + colorStyle + ' href="' + event.url + '"' + klass + '">' + event.title + '</a>' + duedate + '</div>';
+  };
+};
+
+function dateFromString(str) {
+  var datePieces = str.split('/')
+    , today = new Date()
+    , month = today.getMonth()
+    , day = today.getDate()
+    , year = today.getFullYear();
+
+  if (datePieces.length === 1) {
+    day = parseInt(datePieces[0], 10);
   }
+
+  if (datePieces.length === 2) {
+    month = parseInt(datePieces[0], 10) - 1;
+    day = parseInt(datePieces[1], 10);
+  }
+
+  if (datePieces.length === 3) {
+    month = parseInt(datePieces[0], 10) - 1;
+    day = parseInt(datePieces[1], 10);
+    year = parseInt(datePieces[2], 10);
+  }
+
+  return new Date(year, month, day, 0, 0, 0, 0);
 };
