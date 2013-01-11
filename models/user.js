84a01438202124b1a1b80fbda00a7e5d108ab770
numso <dosmun@gmail.com>
2013-01-10 17:08:49 -0700

    aoeu

--- models/user.js
+++ models/user.js
@@ -1,91 +1,82 @@
-var mongodb = require('mongodb');
-
-var dbStuffs = setupMongo();
-var db = dbStuffs.db;
+var mongodb = require('mongodb')
+  , mongourl = setupMongo();
 
 function setupMongo() {
   if (process.env.VCAP_SERVICES) {
-    var env = JSON.parse(process.env.VCAP_SERVICES)
-      , mongo = env['mongodb-1.8'][0]['credentials'];
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+    var mongo = env['mongodb-1.8'][0]['credentials'];
   } else {
     var mongo = {
-      hostname: "localhost",
-      port: 27017,
-      username: "",
-      password: "",
-      db: "workflowy"
-    }
+      "hostname": "localhost",
+      "port": 27017,
+      "username": "",
+      "password": "", 
+      "name": "",
+      "db": "workflowy"
+    };
   }
 
-  var server = new mongodb.Server(mongo.hostname, mongo.port, {});
-  var db = new mongodb.Db(mongo.db, server, {w: 1});
+  var generate_mongo_url = function(obj){
+    obj.hostname = (obj.hostname || 'localhost');
+    obj.port = (obj.port || 27017);
+    obj.db = (obj.db || 'test');
 
-  // return cb(db);
-
-  if (!mongo.username && !mongo.password)
-    return {db: db};
-
-  return {db: db, username: mongo.username, password: mongo.password};
-};
-
-function openDBConnection(cb) {
-  db.open(function (err, client) {
-    if (err) return cb(err);
-
-    if (dbStuffs.password) {
-      client.authenticate(dbStuffs.username, dbStuffs.password, function (err2, data2) {
-        if (err2) return cb(err2);
-        var coll = new mongodb.Collection(data2, 'users');
-        cb(null, coll);
-      });
+    if (obj.username && obj.password) {
+      return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
     } else {
-      var coll = new mongodb.Collection(client, 'users');
-      cb(null, coll);
+      return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
     }
-  });
+  };
+
+  return generate_mongo_url(mongo);
 };
 
 exports.userExists = function (username, cb) {
-  openDBConnection(function (err, coll) {
+  mongodb.connect(mongourl, function (err, conn) {
     if (err) return cb(err);
-
-    coll.findOne({user: username}, function (err, user) {
-      db.close();
+    conn.collection('users', function (err, coll) {
       if (err) return cb(err);
-      if (user) return cb(null, true);
-      return cb(null, false);
+      coll.findOne({user: username}, function (err, user) {
+        if (err) return cb(err);
+        if (user) return cb(null, true);
+        return cb(null, false);
+      });
     });
   });
 };
 
 exports.updateUser = function (username, update, cb) {
-  openDBConnection(function (err, coll) {
+  mongodb.connect(mongourl, function (err, conn) {
     if (err) return cb(err);
-    coll.findAndModify({user: username}, [], {$set: update}, {}, function (err, object) {
-      db.close();
-      return cb(err);
+    conn.collection('users', function (err, coll) {
+      if (err) return cb(err);
+      coll.findAndModify({user: username}, [], {$set: update}, {}, function (err, object) {
+        return cb(err);
+      });
     });
   });
 };
 
 exports.saveNewUser = function (user, cb) {
-  openDBConnection(function (err, coll) {
+  mongodb.connect(mongourl, function (err, conn) {
     if (err) return cb(err);
-
-    coll.insert(user, function (err, docs) {
-      db.close();
-      return cb(err);
+    conn.collection('users', function (err, coll) {
+      if (err) return cb(err);
+      coll.insert(user, function (err, docs) {
+        return cb(err);
+      });
     });
   });
 };
 
 exports.getUser = function (username, cb) {
-  openDBConnection(function (err, coll) {
+  mongodb.connect(mongourl, function (err, conn) {
     if (err) return cb(err);
-
-    coll.findOne({user: username}, function (err, user) {
-      db.close();
-      return cb(err, user);
+    conn.collection('users', function (err, coll) {
+      if (err) return cb(err);
+      coll.findOne({user: username}, function (err, user) {
+        return cb(err, user);
+      });
     });
   });
 };
